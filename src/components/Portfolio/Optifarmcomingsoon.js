98f5928ae903bf364058b619dc4747f1234ff42c
keerthinanjundareddy <keerthana@apprikart.com>
2024-02-20 09:38:23 +0530

    added optifarm coming soon page

--- src/components/Portfolio/Optifarmcomingsoon.js
+++ src/components/Portfolio/Optifarmcomingsoon.js
@@ -1,14 +1,37 @@
 import React from 'react'
 import './Optifarmcomingsoon.css'
+import untactlogo from '../../assets/images/Optifarm (2).png'
+import { useNavigate } from 'react-router-dom'
 
-function Optifarmcomingsoon() {
+
+function Untactcomingsoon() {
+    const navigate=useNavigate();
+    const handleback = () =>{
+        navigate(`/`);
+
+    }
   return (
     <>
-      <div>
-        hiii
-      </div>
+    <div className='untactsection-full-div'>
+    <div className='untact-coming-soon-section-div'>
+        <div className='untact-img-div'>
+        <img src={untactlogo} alt="untact" style={{width:"100%",height:"100%",objectFit:"contain"}} />
+        </div>
+        {/* <div className='newsite-text-div'>OUR NEW SITE IS</div> */}
+    <div className='coming-soon-section'>
+  
+           <div className='coming-txt'>COMMING SOON</div>
+           <div className='soon-txt'>we are soon coming live..</div>
+         
+    </div>
+    <div>
+            <div className='back-to-home-btn' onClick={handleback} >Back to home</div>
+           </div>
+    </div>
+    </div>
     </>
   )
 }
 
-export default Optifarmcomingsoon
+export default Untactcomingsoon
+
